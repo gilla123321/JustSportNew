41d282a88cc7785cfdd73f6ef77edc1ffaac97b2
User <itaigill7@gmail.com>
2024-04-05 09:49:06 +0300

    removed code from short practice activity

--- app/src/main/java/com/itai/justrun/ShortPracticeActivity.java
+++ app/src/main/java/com/itai/justrun/ShortPracticeActivity.java
@@ -17,10 +17,6 @@ public class ShortPracticeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_short_practice);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
