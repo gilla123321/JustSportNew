727d6c189319500dbf853c7a388395395209ee28
User <itaigill7@gmail.com>
2024-04-05 10:14:52 +0300

    added count down timer inside a textview

--- app/src/main/java/com/itai/justrun/ShortPracticeActivity.java
+++ app/src/main/java/com/itai/justrun/ShortPracticeActivity.java
@@ -3,6 +3,7 @@ package com.itai.justrun;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.itai.justrun.R;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class ShortPracticeActivity extends AppCompatActivity {
 
     @Override
@@ -20,16 +24,25 @@ public class ShortPracticeActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_short_practice);
 
+        // Initializing thetextView
+        TextView textView;
+        textView = findViewById (R.id.textView);
+
         new CountDownTimer(30000, 1000) {
 
             public void onTick(long millisUntilFinished) {
-               // mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+                // Used for formatting digit to be in 2 digits only
+                NumberFormat f = new DecimalFormat("00");
+                long hour = (millisUntilFinished / 3600000) % 24;
+                long min = (millisUntilFinished / 60000) % 60;
+                long sec = (millisUntilFinished / 1000) % 60;
+                textView.setText(f.format(hour) + ":" + f.format(min) + ":" + f.format(sec));
                 // logic to set the EditText could go here
-                Log.e("XXXXXX","onTick " + millisUntilFinished );
+                //Log.e("XXXXXX","onTick " + millisUntilFinished );
             }
-
+            // When the task is over it will print 00:00:00 there
             public void onFinish() {
-                //mTextField.setText("done!");
+                textView.setText("00:00:00");
             }
 
         }.start();
